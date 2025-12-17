bacd3c7668ad98fb5eb2b269489c102480bcc459
isaya mbwaga <isayambwaga10@gmail.com>
2025-12-17 16:16:48 +0300

    update description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,22 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "TheKingandthecode",
-  description: "
-The King & The Code is a stage play production by Pastor Tony Osborn Ministries featuring a Swahili drama accompanied with live music, spoken word, dance numbers and so much more.",
+  // Using backticks (``) allows the string to span multiple lines
+  description: `The King & The Code is a stage play production by Pastor Tony Osborn Ministries 
+  featuring a Swahili drama accompanied with live music, spoken word, 
+  dance numbers and so much more.`,
 };
 
 export default function RootLayout({
@@ -16,9 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-       
-      >
+      <body >
         {children}
       </body>
     </html>
